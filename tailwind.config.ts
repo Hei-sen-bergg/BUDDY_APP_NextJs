16132d9db3a7bf5739746a29d91620d175fb46c3
Senraj <senrajshekar98@gmail.com>
2024-09-10 16:41:07 +0530

    clerk authentication

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/plugin.cjs")],
+  darkMode: "class"                              
 };
 export default config;
